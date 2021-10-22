f3de4238ea72bd1a84b6f72d5b0c4b6e9d587370
HwangJubin <ghkdwnqls2281@gmail.com>
2021-10-22 16:16:30 +0900

    gtm

--- src/main/java/api/GTM.java
+++ src/main/java/api/GTM.java
@@ -7,20 +7,32 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.tagmanager.TagManager;
 import com.google.api.services.tagmanager.TagManagerScopes;
 import com.google.api.services.tagmanager.model.Account;
-import lombok.extern.slf4j.Slf4j;
+import com.google.api.services.tagmanager.model.Container;
+import com.google.api.services.tagmanager.model.Folder;
+import com.google.api.services.tagmanager.model.Workspace;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.Collections;
 import java.util.List;
 
-@Slf4j
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class GTM {
-    private static String KEY_FILE_LOCATION = "";
+    private static String KEY_FILE_LOCATION = "key/key_file.json";
 
     private TagManager tagManager;
     private String accountId;
     private String containerId;
+
+    public GTM(String accountId) {
+        this.accountId = accountId;
+    }
 
     /**
      * TagManager 객체 생성
@@ -36,13 +48,45 @@ public class GTM {
         tagManager = new TagManager(httpTransport, GsonFactory.getDefaultInstance(), credential);
     }
 
-    public List<Account> getAccounts() {
-        try {
-            return tagManager.accounts().list().execute().getAccount();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
+    private String getContainerPath() { return "accounts/" + accountId + "/containers/" + containerId; }
+    private String getWorkspacePath() { return "accounts/" + accountId + "/containers/" + containerId + "/workspaces/2"; }
+
+    /* Account
+    * setAccountId(accountId) 호출 후 실행
+    * */
+    public List<Account> getAccounts() throws IOException { return tagManager.accounts().list().execute().getAccount(); }
+    public Account getAccount(String accountId) throws IOException { return tagManager.accounts().get("accounts/" + accountId).execute(); }
+
+    /* Container
+    * setAccountId(accountId), setContainerId(containerId) 호출 후 실행
+    * */
+    public List<Container> getContainers() throws IOException {
+        return tagManager.accounts().containers().list("accounts/" + accountId).execute().getContainer();
+    }
+
+    public Container getContainer() throws IOException {
+        return tagManager.accounts().containers().get(getContainerPath()).execute();
+    }
+
+    public Container createContainer(String name) throws IOException {
+        Container container = new Container();
+        container.setName(name);
+        container.setUsageContext(Collections.singletonList("0"));
+
+        return tagManager.accounts().containers().create("accounts/" + accountId, container).execute();
+    }
+
+    public void deleteContainer() throws IOException { tagManager.accounts().containers().delete(getContainerPath()).execute(); }
+
+    /* folder
+    * setAccountId(accountId), setContainerId(containerId) 호출 후 실행
+    * */
+    public List<Folder> getFolders() throws IOException {
+        return tagManager.accounts().containers().workspaces().folders().list(getWorkspacePath()).execute().getFolder();
+    }
+
+    public Folder getFolder(String folderId) throws IOException {
+        return tagManager.accounts().containers().workspaces().folders().get(getWorkspacePath() + "/folders/" + folderId).execute();
     }
 
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,21 @@
-import lombok.extern.slf4j.Slf4j;
+import api.GTM;
 
-@Slf4j
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class main {
 
-    public main(String ...args) {
+    public static void main(String ...args) throws GeneralSecurityException, IOException {
+        GTM gtm = new GTM();
+        gtm.initialize();
 
+        gtm.getAccounts().forEach(i -> System.out.println(i.toString()));
+        System.out.println(gtm.getAccount("6005040042"));
+
+        gtm.setAccountId("6004589006");
+        gtm.getContainers().forEach(i -> System.out.println(i.toString()));
+
+        System.out.println(gtm.getContainer());
     }
+
 }
